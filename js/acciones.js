4f8fff58bed93223b5aafc6af70dfe0e6641345f
FatimaFlores <fattimadsaupoo@gmail.com>
2016-05-18 09:32:59 -0500

    4subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,48 +1,58 @@
 // JavaScript Document
 $(document).ready(function(e){
-//watchID se refiere a actual
-var watchID=null;
-document.addEventListener("deviceready",Dispositivo_listo,false);
-//cuando esta listo el Dipositivo
-function Dispositivo_listo(){
-Comienza();
-}
-//Empieza la Observacion de la Aceleracion
-function Comienza(){
-	//Actualizar la Aceleracion cada 2 segundos //
-var opciones={frequency:2000};
-watchID=navigator.accelerometer.watchAcceleration(Correcto,Erros,opciones);
-navigator.geolocation.getCurrentPosition(localiza ,ErrorLocalizacion);
-}
-//detiene la Observacion de la Aceleracion
-function Detente(){
-	if(watchID){
-	navigator.accelerometer.clearWatch (watchID);
-	watchID=null;
+	//watchID se refiere a actual
+	
+	var watchID=null;
+	document.addEventListener("deviceready",Dispositivo_Listo,false);
+	
+	//Cuando esta listo el dispositivo
+	function Dispositivo_Listo(){
+		Comienza();
 	}
-}
-//Corecto:toma una Captura de la aceleracion
-function Correcto(acceleration){
-	var element=document.getElementById('acelerometro');
-element.innerHTML='Aceleracion en X:'+acceleration.x+'<br/>'+
-'Aceleracion en Y:' +acceleration.y+'<br/>';
-'intervalo:'+acceleration.timestamp+'<br/>';
-}
-//erros:Falla al Obtener la Aceleracion
-function Error(){
-	alert('Error!');
-}
-//Exito al localizar
-function localiza(posicion){
-	var element=document.getElementById('geolocalizacion');
-	element.innerHTML='Latitud:' +posicion.coords.latitude +'<br/>' +
-	'Longitud:' +posicion.coords.longitude + '<br/>' +
-	'Precision:' +posicion.coords.accuracy +'<br/>' +
-	'Intervalo:' +posicion.timestamp +'<br/>';
-}
-//Error en la Geolocalizacion
-function ErrorLocalizacion(error){
-	alert('codigo:' +error.code +'\n'+
-	'mensaje:'+error.message+'\n');
-}
-});
+	
+	//Empieza la observacion de la aceleracion
+	function Comienza(){
+		
+		//Actualiza la aceleracion cada 2 segundos
+		//
+		var opciones={frequency:2000};
+		
+		watchID=navigator.accelerometer.watchAcceleration(Correcto,Error,opciones);
+navigator.geolocation.getCurrentPosition(Localiza,ErrorLocalizacion);
+	}
+	//Detiene la observacion de la acelerecion
+	
+	function Detente(){
+		if (watchID){
+			navigator.accelerometer.clearWatch (watchID);
+			watchID=null;
+		}
+	}
+	//Correcto: Toma una captura de la aceleracion
+	function Correcto(acceleration){
+		var element=document.getElementById('acelerometro');
+		
+		element.innerHTML='Aceleración en X:'+acceleration.x+'<br/>'+
+		'Aceleración en Y:'+acceleration.y+'<br/>'+ 
+		'Intervalo:'+acceleration.timestamp+'<br/>';
+	}
+	
+	//eRROR:FALLA al obtener la aceleracion
+	function Error(){
+		alert('Error!');
+	}
+	//Exito al localizar
+	function Localiza(posicion){
+		var element=document.getElementById('geolocalizacion');
+		element.innerHTML='Latitud:'+posicion.coords.latitude+'<br/>'+
+		'Longitud:'+posicion.coords.longitude+'<br/>'+
+		'Precisión:'+posicion.coords.accuracy+'<br/>'+
+		'Intervalo:'+posicion.coords.timestamp+'<br/>';
+	}
+	//Error en la geolocalizacion
+	function ErrorLocalizacion(error){
+		alert('codigo:'+error.code+'\n'+
+		'mensaje:'+error.message+'\n');
+	}
+});//document ready
+	
